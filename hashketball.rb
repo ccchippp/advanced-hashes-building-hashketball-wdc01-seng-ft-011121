c2b1c0c969cbc2c6a342dc3bf18686d7fbc050e3
Chip Lempke <clempke90@gmail.com>
2020-12-08 22:10:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,20 @@
 # Write your code here!
+game_hash = {
+  :home => {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :player => {}
+  },
+  :away => {
+    :team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
+    :player => {
+      :name => [
+        "Jeff Adrien",
+        "Bismack Biyombo",
+        "DeSagna Diop",
+        "Ben Gordon",
+        "Kemba Walker"]
+    }
+  }
+}
